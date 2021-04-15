9b07ae1b913d17947dfb21bb040d58083b0721b4
moisesdolores <62561953+moisesdolores@users.noreply.github.com>
2021-04-15 11:53:13 -0400

    Business routes (#2)
    * Migrate posts (#15)
    
    * initializes node server and adds packages (#3)
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * creates migrations for posts table
    
    Co-authored-by: weilyl <64621097+weilyl@users.noreply.github.com>
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * creates migrations for comments
    
    * Migrate comments (#16)
    
    * initializes node server and adds packages (#3)
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * Migrate posts (#15) (#1)
    
    * initializes node server and adds packages (#3)
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * creates migrations for posts table
    
    Co-authored-by: weilyl <64621097+weilyl@users.noreply.github.com>
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    Co-authored-by: weilyl <64621097+weilyl@users.noreply.github.com>
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * creates migrations for comments
    
    Co-authored-by: weilyl <64621097+weilyl@users.noreply.github.com>
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * Business router (#18)
    
    * outlines routes for business router
    
    * adds business router to server.js
    
    * adds comments for new business route endpoints
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    
    * Controllers done and in need of testing (#20)
    
    Co-authored-by: Nila <Nila@LAPTOP-M0QERO78>
    
    * fixes syntax errors in controllers
    
    * adds routes for businesses
    
    * adds routes for businesses
    
    Co-authored-by: Weily Lang <weily.lang@gmail.com>
    Co-authored-by: weilyl <64621097+weilyl@users.noreply.github.com>
    Co-authored-by: Nila Singh <77280258+NilaSingh@users.noreply.github.com>
    Co-authored-by: Nila <Nila@LAPTOP-M0QERO78>

---
+++ routes/business.js
@@ -0,0 +1,73 @@
+const { Router } = require("express");
+const router = new Router();
+const {
+  getAllBusinesses,
+  locateBusiness,
+  getBusinessByName,
+  getABusiness,
+  createBusiness,
+  updateBusiness,
+  deleteBusiness,
+} = require("../controllers/Business");
+
+const {
+  getAComment,
+  getAllComments,
+  getBusinessComments,
+  getPostComments,
+  createComment,
+  updateComment,
+  deleteComment,
+} = require("../controllers/Comments");
+
+const {
+  getAPost,
+  getBusinessPosts,
+  getEveryPost,
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/Posts");
+// business related routes
+
+// register a new business
+router.post("/register", createBusiness);
+// business login
+router.post("/login");
+
+// business own profile
+router.get("/:id", getABusiness);
+
+// get all businesses
+router.get("/all", getAllBusinesses);
+
+// get all businesses by type
+router.get("/:type");
+
+// get all businesses by type & location
+router.get("/:type/rad=?:distance");
+
+// delete a business
+router.delete("/:id/delete", deleteBusiness);
+
+// business makes a post
+router.post("/createPost", createPost);
+
+// business comments on a post
+router.post("/:postId", createComment);
+
+// business updates a post
+router.put("/:postId", updatePost);
+// business updates a comment
+router.put("/:commentId", updateComment);
+
+// business deletes a post
+router.delete("/:postId", deletePost);
+
+// business deletes a comment it made or a comment on a post it made
+router.delete("/:postId/:commentsId", deleteComment);
+
+// search engine for business name, description
+router.get("/search?:query", getBusinessByName);
+
+module.exports = router;
